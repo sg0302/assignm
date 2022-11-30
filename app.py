8f103a6e9c534fc5c1a0d45812b6b026fbf3f7b9
sg0302 <119248637+sg0302@users.noreply.github.com>
2022-11-30 19:27:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+title = "subtract 2 numbers"
+st.set_page_config(page_title=title)
+
+st.title('division of 2 numbers')
+
+x = st.number_input('Enter first number')
+y = st.number_input('Enter second number')
+if: y!=0:
+  st.write(not defined)
+else:
+  z=x/y
+st.write(z,"is the result")
